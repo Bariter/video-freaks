3775c4f352751cfcae38e2221785dcf24a2507f7
kkas <kentakikui@gmail.com>
2015-06-21 16:47:58 +0900

    Add display tags.

--- app/helpers/adults_helper.rb
+++ app/helpers/adults_helper.rb
@@ -12,4 +12,17 @@ module AdultsHelper
   def reverse_percentage(str)
     return (100 - str.to_i).to_s + "%"
   end
+
+  def show_movie_tags(tag_arys)
+    tags = "";
+
+    return "none" if tag_arys.empty?
+
+    tag_arys.each do |tag|
+      tags << tag
+      tags << ", "
+    end
+
+    return tags.chop!;
+  end
 end
