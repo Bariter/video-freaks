a7f905cab1a27d547a92535516b8b4a68c3a6e49
kkas <kentakikui@gmail.com>
2015-06-28 22:43:21 +0900

    Add config/unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+#@app_path = '/vagrant/video-freaks'
+#@app_path = '/home/vagrant/video-freaks'
+require 'etc'
+cur_user_home_dir = Etc.getpwuid.dir
+@app_path = "#{cur_user_home_dir}/video-freaks"
+puts @app_path
+
+worker_processes 2
+working_directory "#{@app_path}/"
+preload_app true
+timeout 30
+listen "/tmp/unicorn.sock", :backlog => 64
+pid "/tmp/unicorn.pid"
+stderr_path "#{@app_path}/log/unicorn.stderr.log"
+stdout_path "#{@app_path}/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+  sleep 1
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
