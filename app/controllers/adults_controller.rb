5d6a971b4fa76a589d7f3fb1e0a952239781c480
kkas <kentakikui@gmail.com>
2015-09-13 15:22:03 +0900

    Move creating video resource from AdultsController to UrlsController

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -9,33 +9,4 @@ class AdultsController < ApplicationController
     @selected_url_tags = UrlTag.all
     @thumbs = Thumbnail.without(:image).all
   end
-
-  def create
-    url = Url.create(
-      site_name:    params["Sitename"],
-      origin_url:   params["OriginUrl"],
-      movie_pics:   params["moviePics"],
-      title:        params["title"],
-      time:         params["time"],
-      player_url:   params["playerurl"],
-      emb_url:      params["emburl"],
-      rate:         params["rate:"],
-      rating_count: params["ratingcount"].to_i,
-      rate_HTML:    params["ratehtml"],
-      mosaic:       params["mosaicflg"] === "0" ? false : true
-    )
-
-    params["tags"] ||= []
-    params["tags"].each do |t|
-      tag = Tag.find_or_create_by(
-        tag_name: t
-      )
-      url_tag = UrlTag.create(
-        url_id: url["_id"],
-        tag_id: tag["_id"]
-      )
-    end
-
-    render :nothing => true
-  end
 end
